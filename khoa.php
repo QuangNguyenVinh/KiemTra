746ea332eb798270a5980e9737848906c8bf61cd
=VQuang <vinhquangnguyen2@gmail.com>
2019-11-16 09:41:23 +0700

    initial commit

--- khoa.php
+++ khoa.php
@@ -11,11 +11,12 @@
         require('connect.php');
     ?>
     <form action="" method="POST">
-        <table>
+        <table align="center">
+            <h3 align="center">DANH SÁCH LỚP</h3>
             <tr>
-                <td align="center">Khoa: <select name="khoa" id="khoa">
+                <td align="center">Khoa: <select name="ds_khoa" id="khoa">
                     <?php
-                        $sql = "SELECT * FROM khoa";
+                        $sql = "SELECT * FROM khoa ";
                         $result = mysqli_query($conn, $sql);
                         if(mysqli_num_rows($result) > 0)
                         {
@@ -24,7 +25,7 @@
                                $ma_khoa = $row['ma_khoa'];
                                $ten_khoa = $row['ten_khoa'];
                                echo '<option value="'.$ma_khoa.'"';
-                               if(isset($_REQUEST['khoa']) && ($_REQUEST['khoa']==$ma_khoa))
+                               if(isset($_REQUEST['ds_khoa']) && ($_REQUEST['ds_khoa']==$ma_khoa))
                                {
                                     echo 'selected="selected"';
                                } 
@@ -34,8 +35,47 @@
                         mysqli_free_result($result);
                     ?>
                     </select>
+                    <input type="submit" value="Tìm kiếm" name="searchBtn" id="searchBtn"> 
             </tr>
+            
         </table>
+        <?php
+            if(isset($_POST["searchBtn"]))
+            {
+                $ma_khoa = $_REQUEST["ds_khoa"];
+                $sql_search = "SELECT ma_lop, ten_lop, si_so, k.ten_khoa, gv.ho_gv , gv.ten_gv 
+                            FROM lop AS l JOIN khoa AS k ON l.ma_khoa = k.ma_khoa
+                            JOIN giangvien AS gv ON l.ma_gvcv = gv.ma_gv
+                            WHERE l.ma_khoa = '".$ma_khoa."' ";
+                            ;
+                 $res_search = mysqli_query($conn, $sql_search);
+                 if(mysqli_num_rows($res_search) > 0)
+                 {
+                    echo '<table border="1" align="center">';
+                    echo '<tr>';
+                    echo '<th> Mã lớp </th>';
+                    echo '<th> Tên lớp </th>';
+                    echo '<th> Sĩ số </th>';
+                    echo '<th> Tên khoa </th>';
+                    echo '<th> Họ GVCV </th>';
+                    echo '<th> Tên GVCV </th>';
+                    echo '</tr>';
+                     while($row = mysqli_fetch_object($res_search))
+                     {
+                         echo '<tr>';
+                         echo '<td <a href="capnhat.php?='.$row->ma_lop.'">'.$row->ma_lop.'</td>';
+                         echo '<td>'.$row->ten_lop.'</td>';
+                         echo '<td>'.$row->si_so.'</td>';
+                         echo '<td>'.$row->ten_khoa.'</td>';
+                         echo '<td>'.$row->ho_gv.'</td>';
+                         echo '<td>'.$row->ten_gv.'</td>';
+                         echo '</tr>';
+
+                     }
+                     echo '</table>';
+                 }
+            }
+        ?>
     </form>
 </body>
 </html>
